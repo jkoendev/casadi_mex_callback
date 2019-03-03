269e3d5049ed3329d4ff67c808662a3725394c19
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-03 17:34:56 +0100

    assert and input checks

--- casadi_mex.cc
+++ casadi_mex.cc
@@ -11,9 +11,28 @@
 
 #include "casadi/casadi.hpp"
 
+void display(const std::string str)
+{
+  mexPrintf("%s\n", str.c_str());
+}
+
+void mexError(std::string str)
+{
+  mexErrMsgIdAndTxt("casadi_mex:mexError", str.c_str());
+}
+
+void mexAssert(bool cond, std::string str)
+{
+  if (!cond)
+  {
+    mexError(str);
+  }
+}
 
 std::string to_string(const mxArray* in_arr)
 {
+  mexAssert(mxIsChar(in_arr), "Argument must be string.");
+
   size_t n_chars = mxGetN(in_arr);
   char fcn_name[n_chars+1];
   mxGetString(in_arr, fcn_name, n_chars+1);
@@ -22,7 +41,7 @@ std::string to_string(const mxArray* in_arr)
 
 std::vector<double> to_numeric_vec(const mxArray* in_arr)
 {
-  assert(mxIsDouble(in_arr) && !mxIsComplex(in_arr));
+  mexAssert(mxIsDouble(in_arr) && !mxIsComplex(in_arr), "Invalid convertion from mx type to vector.");
 
   int nv = (int) mxGetNumberOfElements(in_arr);
   std::vector<double> vec(nv);
@@ -44,27 +63,38 @@ std::vector<double> to_numeric_vec(const mxArray* in_arr)
 void mexFunction(int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[])
 {
+
+  mexAssert(nrhs >=  1, "Not enough input arguments.");
+
   // First input is always the function identifier as string
   std::string fcn_name = to_string(prhs[0]);
 
   if (fcn_name.compare("create") == 0) {
 
     // call args are:
-    //  prhs[0] "create" : str
-    //  prhs[1] name : str
-    //  prhs[2] shape : matrix
+    //  prhs[0] "create" : string
+    //  prhs[1] name : string
+    //  prhs[2] shape : numeric
+
+    mexAssert(nrhs >= 3, "Not enough input arguments.");
 
     std::string name = to_string(prhs[1]);
     std::vector<double> shape = to_numeric_vec(prhs[2]);
 
-    assert(shape.size() == 2);
+    mexAssert(shape.size() == 2, "Shape must be of length 2.");
     casadi::SX var = casadi::SX::sym(name, (int)shape[0], (int)shape[1]);
 
-    mexPrintf("testestset\n");
-    mexPrintf("yeyeyeeye %s \n", name.c_str());
+    std::stringstream out_str;
+
+    out_str << name << std::endl;
+    out_str << var << std::endl;
+
+    display(out_str.str());
 
 
-  } else if (fcn_name.compare("create") == 0) {
+  } else if (fcn_name.compare("call") == 0) {
 
+  } else {
+    mexError("Command not recognized.");
   }
 }
